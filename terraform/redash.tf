6cea64810348dfcb0389d9a1367c7e6319a991b9
Tristan Handy <jthandy@gmail.com>
2016-05-04 08:39:06 -0400

    add redash backend RDS

--- terraform/redash.tf
+++ terraform/redash.tf
@@ -16,6 +16,34 @@ resource "aws_instance" "redash1" {
   }
 }
 
+resource "aws_db_subnet_group" "public" {
+  name = "public"
+  description = "Subnet group for publicly accessible RDS instances"
+  subnet_ids = [
+    "${aws_subnet.public_us_east_1a.id}",
+    "${aws_subnet.public_us_east_1b.id}"
+  ]
+  tags {
+    Name = "public"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "postgres"
+  engine_version       = "9.3"
+  instance_class       = "db.t2.micro"
+  name                 = "redash_backend"
+  username             = "${var.redash_backend_username}"
+  password             = "${var.redash_backend_password}"
+  db_subnet_group_name = "my_database_subnet_group"
+  parameter_group_name = "default.postgres9.3"
+  vpc_security_group_ids = [
+    "${aws_security_group.whitelisted_postgres.id}"
+  ]
+  publicly_accessible = true
+}
+
 resource "aws_key_pair" "master" {
   key_name = "master"
   public_key = "${file(var.public_key_path)}"
--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -166,3 +166,25 @@ resource "aws_security_group" "whitelisted_ssh" {
 	cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "whitelisted_postgres" {
+  name = "whitelisted_postgres"
+  description = "Postgres Access for Whitelisted IPs"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+	from_port = 5432
+	to_port = 5432
+	protocol = "tcp"
+	cidr_blocks = [
+	  "${split(",", var.whitelisted_ips)}"
+	]
+  }
+
+  egress {
+	from_port = 0
+	to_port = 0
+	protocol = "-1"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+}
